a8aef4ba791b4c527ba77e38cbadf7973f96a3b9
Mikael Fremling <micke.fremling@gmail.com>
2023-03-24 16:25:21 +0100

    Add the fists files.

---
+++ src/MajoranaWolfReps.jl
@@ -0,0 +1,17 @@
+if !@isdefined(TabLevel)
+    TabLevel = ""
+end
+println(TabLevel*"Open MajoranaWolfReps.jl")
+TabLevel=TabLevel*"    "
+
+function ScaleToStr(scale::Union{MathLink.WExpr,MathLink.WSymbol},tex::Bool,KetState::Bool,NumOps::Int,FirstTerm::Bool)
+    if FirstTerm
+        return "$(scale)*"
+    else
+        return "+ $(scale)*"
+    end
+end
+
+
+TabLevel=TabLevel[1:end-4]
+println(TabLevel*"Close MajoranaWolfReps.jl")
---
+++ test/runtests.jl
@@ -0,0 +1,22 @@
+#include("../src/MajoranaReps.jl")
+using MajoranaReps
+using MathLink
+using Test
+
+
+@testset "Majorana Operators and MathLink Expressions" begin
+    @test W"a" * Ket() == Ket() * W"a"
+    @test W"a" * c(1) == c(1) * W"a"
+
+    @test W"a" * c(1) + W"b" * c(2) ==  W"b" * c(2) +W"a" * c(1)
+
+
+    @test W"a"*bx(1) == W"a"*bx(1)
+    @test W"a"*W"b"*bx(1) == W`a b`*bx(1)
+    
+    
+    @test W"a"*bx(1)*Ket() == W"a"*bx(1)*Ket()
+    @test W"a"*W"b"*bx(1)*Ket() == W`a b`*bx(1)*Ket()
+    
+end
+
